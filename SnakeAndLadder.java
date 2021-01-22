da1ef4eaff9d5630bb1ff1e8af2617d43d494682
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-21 22:58:18 -0800

    Snake And Ladder UC7 part done

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,36 +2,69 @@ class SnakeAndLadder
 {
    public static void main(String args[])
    {
-      int pos=0,no_play=0,ladder=1,snake=2,count=0;
+      int pos=0,pos1=0,pos2=0,no_play=0,ladder=1,snake=2;
       System.out.println("Welcome to Snake And Ladder Problem");
-   	while(pos!=100)
+   	while(pos1!=100 && pos2!=100)
 		{
-	   	int check=(int) Math.floor(((Math.random()*10)%6)+1);
-      	count++;
-			int moves=(int) Math.floor((Math.random()*10)%3);
-      	if(moves == no_play)
+	   	int check1=(int) Math.floor(((Math.random()*10)%6)+1);
+			int moves1=(int) Math.floor((Math.random()*10)%3);
+			int check2=(int) Math.floor(((Math.random()*10)%6)+1);
+         int moves2=(int) Math.floor((Math.random()*10)%3);
+
+      	if(moves1 == no_play)
       	{
-         	pos=pos;
+         	pos1=pos1;
       	}
-      	else if (moves == ladder)
+      	else if (moves1 == ladder)
       	{
-         	pos=pos+check;
+         	pos1=pos1+check1;
       	}
-      	else if (moves == snake)
+      	else if (moves1 == snake)
       	{
-         	pos=pos-check;
+         	pos1=pos1-check1;
       	}
-			else if (pos<0)
+			else if (pos1<0)
 			{
-				pos=0;
+				pos1=0;
 			}
-			if (pos>100)
+			if (pos1>100)
 			{
-				pos=pos;
+				pos1=pos1;
 			}
+
+
+			 if(moves2 == no_play)
+         {
+            pos2=pos2;
+         }
+         else if (moves2 == ladder)
+         {
+            pos2=pos2+check2;
+         }
+         else if (moves2 == snake)
+         {
+            pos2=pos2-check2;
+         }
+         else if (pos2<0)
+         {
+            pos2=0;
+         }
+         if (pos2>100)
+         {
+            pos2=pos2;
+         }
+
 		}
-		System.out.println("the number of times the dice was played to win : " +count);
-      
-     	System.out.println("The final position of the player is : " +pos);
+		System.out.println("1st Player position is : " +pos1);
+		System.out.println("2nd Player position is : " +pos2);
+		if(pos1==100)
+      {
+         System.out.println("1st Player win");
+      }
+      else if(pos2==100)
+      {
+         System.out.println("2nd Player win");
+      }
+
    }
 }
