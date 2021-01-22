93673a656933b25b7f91a473668888a90fb46a39
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-21 21:32:17 -0800

    Snake And Ladder UC2 part done

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,5 +4,7 @@ class SnakeAndLadder
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Snake And Ladder Problem");
+		int check=(int) Math.floor(((Math.random()*10)%6)+1);
+		System.out.println("The number is : " +check);
 	}
 }
