ed8f9ab7d9c90d8ba4ee714332055548f049f2d1
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-21 22:03:44 -0800

    Snake And Ladder UC4 part done

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,22 +4,27 @@ class SnakeAndLadder
    {
       int pos=0,no_play=0,ladder=1,snake=2;
       System.out.println("Welcome to Snake And Ladder Problem");
-      int check=(int) Math.floor(((Math.random()*10)%6)+1);
-      int moves=(int) Math.floor((Math.random()*10)%3);
-      if(moves == no_play)
-      {
-         pos=pos;
-      }
-      else if (moves == ladder)
-      {
-         pos=pos+check;
-      }
-      else if (moves == snake)
-      {
-         pos=pos-check;
-      }
-      System.out.println("The number is : " +check);
-      System.out.println("The move player gets :" +moves);
-      System.out.println("The current position of the player is : " +pos);
+   	while(pos!=100)
+		{
+	   	int check=(int) Math.floor(((Math.random()*10)%6)+1);
+      	int moves=(int) Math.floor((Math.random()*10)%3);
+      	if(moves == no_play)
+      	{
+         	pos=pos;
+      	}
+      	else if (moves == ladder)
+      	{
+         	pos=pos+check;
+      	}
+      	else if (moves == snake)
+      	{
+         	pos=pos-check;
+      	}
+			else if (pos<0)
+			{
+				pos=0;
+			}
+		}
+     	System.out.println("The current position of the player is : " +pos);
    }
 }
